4e30f34a6479ee7021ee3283268a94fb350378cb
Tony <tony.js@zoho.eu>
2020-11-30 14:14:50 -0300

    feat: upgrade typings and add under properties

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
-import React, { ElementType, forwardRef, RefAttributes, ComponentType } from 'react'
+import React, { ElementType, forwardRef, RefAttributes, ComponentType, ComponentClass, FunctionComponent, useMemo, LegacyRef } from 'react'
 
 type GetProps<E> = E extends keyof JSX.IntrinsicElements
-  ? JSX.IntrinsicElements[E] : E extends ComponentType<infer P> ? P : never
+  ? JSX.IntrinsicElements[E]
+  : E extends ComponentType<infer P> ?
+    E extends forwardComponent.ForwardedComponentExoticComponent<infer E, infer P>
+      ? forwardComponent.CreateProps<E, P>
+      : P
+  : never
 
-type GetElement<E> = E extends keyof JSX.IntrinsicElements
-  ? JSX.IntrinsicElements[E] : E
+type GetRef<E> = E extends keyof JSX.IntrinsicElements
+  ? JSX.IntrinsicElements[E]['ref'] extends LegacyRef<infer A>
+    ? A
+    : never
+  : E extends ComponentClass<any>
+    ? E
+    : E extends FunctionComponent<any>
+      ? GetProps<E> extends RefAttributes<infer A>
+        ? A
+        : GetProps<E> extends React.HTMLAttributes<infer El>
+          ? El
+          : never
+      : never
 
-type Types = keyof JSX.IntrinsicElements | ComponentType
+type Types = keyof JSX.IntrinsicElements | ComponentType<any>
 
 type forwardComponent =
   <E extends Types, P>(
@@ -14,27 +30,54 @@ type forwardComponent =
     render: forwardComponent.RenderEl<E, P>
   ) => forwardComponent.ForwardedComponentExoticComponent<E, P>
 
+const regexp = /^_/
+
 const forwardComponent: forwardComponent = ( initial, render ) => {
+  // @ts-ignore
   return forwardRef<any, any>( ( { as, ...props }, ref ) => {
 
-    return render( props, as ?? initial, ref )
+    const division = useMemo( () => {
+      return Object.entries( props ).reduce( ( division, [ key, value ] ) => {
+        if ( regexp.test( key ) ) division.under[key.replace( regexp, '' )] = value
+        else division.direct[key] = value
+        return division
+      }, {
+        under: {} as { [k: string]: any },
+        direct: {} as { [k: string]: any }
+      } )
+    }, [ props ] )
+
+    return render( division.direct as any, division.under as any, as ?? initial, ref )
 
   } ) as forwardComponent.ForwardedComponentExoticComponent<any, any>
 }
 
 namespace forwardComponent {
-  export type RenderEl<E extends Types, P> = Render<GetProps<E> & P>
-  export interface Render<P> {
-    ( props: P, Component: ComponentType<any>, ref: React.ForwardedRef<any> ): React.ReactElement | null
+  export type RenderEl<E extends Types, P> = Render<E, GetProps<E> & P>
+  export interface Render<A extends Types, P> {
+    (
+      props: P,
+      underProps: GetProps<A>,
+      Component: ComponentType<any>,
+      ref: React.ForwardedRef<any>
+    ): React.ReactElement | null
   }
 
-  type DefaultProp<A extends Types> = { as?: A, ref?: RefAttributes<GetElement<A>> }
+  type DefaultProps<A extends Types> = { as?: A } & RefAttributes<GetRef<A>>
 
-  type Props<A extends Types, P> =
-    & Omit<GetProps<A>, keyof P>
+  type UnderProps<A extends Types> = {
+    [K in keyof Omit<GetProps<A>, 'ref'> as `_${string & K}`]+?: GetProps<A>[K]
+  }
+
+  type Props<A extends Types, P> = CreateProps<A, P>
+
+  export type CreateProps<A extends Types, P> =
+    & Omit<GetProps<A>, keyof P | 'as' | 'ref'>
     & P
-    & DefaultProp<A>
+    & DefaultProps<A>
+    & UnderProps<A>
   export interface ForwardedComponentExoticComponent<E extends Types, P> {
+    // @ts-ignore
     <A extends Types = E>( props: Props<A, P> ): React.ReactElement
     defaultProps?: Partial<E extends ElementType<infer P> ? P : never>
     propTypes?: React.WeakValidationMap<P>
