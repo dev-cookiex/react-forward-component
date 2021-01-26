de710783448f17a318818f2802355044531940ad
Tony <tony.js@zoho.eu>
2021-01-26 17:31:07 -0300

    fix: reference typing

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { ComponentType, forwardRef, RefAttributes } from 'react'
 
 const forwardComponent = <P extends any, R = undefined, D extends forwardComponent.AnyElement = 'div'>(
   render: forwardComponent.Render<P>,
-  defaultElement?: D
+  defaultElement: D = 'div' as D
 ): forwardComponent.ExoticComponent<P, R, D> =>
   forwardRef<any, any>(
     ( { as: Component = defaultElement, ...props }, ref ) => render( Component, props, ref )
@@ -26,7 +26,21 @@ namespace forwardComponent {
   type AsProp<E extends AnyElement> = { as?: E }
   type StandardProp<P, E extends AnyElement, R> = P & AsProp<E> & Ref<E, R>
 
-  type Ref<E extends AnyElement, R> = R extends undefined ? RefAttributes<E> : RefAttributes<R>
+  type GetRef<E extends AnyElement> = E extends keyof JSX.IntrinsicElements
+    ? JSX.IntrinsicElements[E] extends React.DetailedHTMLProps<unknown, infer B>
+      ? B
+      : JSX.IntrinsicElements[E] extends React.SVGProps<infer Element>
+        ? Element
+        : never
+    : E extends React.ComponentClass<any, any>
+      ? E
+      : E extends React.FunctionComponent<infer P>
+        ? P extends RefAttributes<infer R>
+          ? R
+          : never
+        : never
+
+  type Ref<E extends AnyElement, R> = R extends undefined ? RefAttributes<GetRef<E>> : RefAttributes<R>
 
   export type Props<P extends AnyProps, E extends AnyElement, R> =
     Omit<GetAnyElementProp<E>, keyof StandardProp<P, E, R>> & StandardProp<P, E, R>
